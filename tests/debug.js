79bc1307ac58d3b33f68191ee645d0f4acd5bfe6
capinemo <capitan__nemo@mail.ru>
2019-03-26 01:31:09 +0300

    Moved temporary debug file to tests folder

---
+++ tests/debug.js
@@ -0,0 +1,19 @@
+//= tests.js
+
+// = core/core.functions.js
+
+// = inject/inject.js
+
+// = lang/lang.js
+
+// = debug/debug.js
+
+// = runner/runner.js
+
+// = iface/iface.js
+
+// = editor/editor.js
+
+// = server/server.js
+
+// = unit/unit.js
