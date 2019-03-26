0513a51ca34d6c8004b23d5a5e7ba1a2fb644c7e
capinemo <capitan__nemo@mail.ru>
2019-03-26 23:34:51 +0300

    Deleted old files from tests

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,8 @@
-const facade = require("./atouch/facade");
-const FACADE = facade.FACADE;
+const assert = require('assert');
+const inject = require('./list/inject');
+const atouch = require('./list/atouch');
 
-let Facade = new FACADE();
+describe('Atouch:', function () {
+    inject();
+    atouch();
+});
