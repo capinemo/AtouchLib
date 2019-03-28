30ed67d9d9b3a84fa45022066ca8d26fe6e9d44d
capinemo <capitan__nemo@mail.ru>
2019-03-28 12:42:39 +0300

    Added description and parameter validation in Test class

--- src/js/test/test.js
+++ src/js/test/test.js
@@ -1,7 +1,7 @@
 /**
  * @class TEST
- * @description Atouch class generating test via script
- * @version 0.0.1
+ * @description Class for saving data of the single test 
+ * @version 0.1.0
  */
 let TEST = (function () {
     let id = '',
@@ -9,7 +9,15 @@ let TEST = (function () {
         desc = '',
         chain = [];
 
-
+    /**
+     * Clear all data in the current test
+     *
+     * @public
+     *
+     * @param {string} name             Service name
+     * @param {Object} classObject      Object registered as service
+     * @returns {TEST}                  TEST object
+     */
     TEST.prototype.reset = function () {
         id = '';
         name = '';
@@ -20,22 +28,46 @@ let TEST = (function () {
     };
 
     /**
-     * Running test selected in interface
+     * Sets id for new test
      *
      * @public
      *
-     * @param {string} test_id          Selected test id
-     * @returns {none}                  No return
+     * @param {string} test_id          New test id
+     * @returns {TEST}                  TEST object
      */
     TEST.prototype.id = function (test_id) {
-        // TODO sanitize and filter
-
+        if (typeof test_id !== 'string') {
+            throw new Error('Test.id: not string given in parameter');
+        }
+        
+        if (test_id === '') {
+            test_id = genUUID();
+        }
+        
+        test_id = filterVariable(test_id, 'a-zA-Z0-9\-\_');
+        
         id = test_id;
         return this;
     };
 
+    /**
+     * Sets name for new test
+     *
+     * @public
+     *
+     * @param {string} test_name        New test name
+     * @returns {TEST}                  TEST object
+     */
     TEST.prototype.name = function (test_name) {
-        // TODO sanitize and filter
+        if (typeof test_name !== 'string') {
+            throw new Error('Test.name: not string given in parameter');
+        }
+        
+        if (test_name === '') {
+            throw new Error('Test.name: empty string given');
+        }
+        
+        test_name = filterVariable(test_name, 'a-zA-Z0-9\-\_');
 
         name = test_name;
 
@@ -46,32 +78,88 @@ let TEST = (function () {
         return this;
     };
 
+    /**
+     * Sets description for new test
+     *
+     * @public
+     *
+     * @param {string} test_desc        New name description
+     * @returns {TEST}                  TEST object
+     */
     TEST.prototype.desc = function (test_desc) {
-        // TODO sanitize and filter
+        if (typeof test_desc !== 'string') {
+            throw new Error('Test.description: not string given in parameter');
+        }
+        
+        test_desc = filterVariable(test_desc, 'a-zA-Zа-яА-ЯёЁ0-9\-\_\.\, ');
+
 
         desc = test_desc;
         return this;
     };
 
+    /**
+     * Sets chain of commands for new test. Chain of commands receives from <br />
+     * method Atouch.getCollectedTasks as Array.
+     *
+     * @public
+     *
+     * @param {ATOUCH} atouch           ATOUCH object
+     * @returns {TEST}                  TEST object
+     */
     TEST.prototype.chain = function (atouch) {
-        // TODO sanitize and filter
+        if (!(atouch instanceof ATOUCH)) {
+            throw new Error('Test.chain: not Atouch object given in parameter');
+        }
 
-        //chain = atouch.getCollectedTasks();
+        if (typeof atouch.getCollectedTasks !== 'function') {
+            throw new Error('Test.chain: invalid interface of Atouch object. Need a getCollectedTasks function');
+        }
+
+        chain = atouch.getCollectedTasks();
         return this;
     };
 
+    /**
+     * Gets id of the current test
+     *
+     * @public
+     *
+     * @returns {string}                Current test id
+     */
     TEST.prototype.getId = function () {
         return id;
     };
 
+    /**
+     * Gets name of the current test
+     *
+     * @public
+     *
+     * @returns {string}                Current test name
+     */
     TEST.prototype.getName = function () {
         return name;
     };
 
+    /**
+     * Gets description of the current test
+     *
+     * @public
+     *
+     * @returns {string}                Current test description
+     */
     TEST.prototype.getDesc = function () {
         return desc;
     };
 
+    /**
+     * Gets commands list of the current test
+     *
+     * @public
+     *
+     * @returns {Array}                 Current test chain
+     */
     TEST.prototype.getChain = function () {
         return chain;
     };
