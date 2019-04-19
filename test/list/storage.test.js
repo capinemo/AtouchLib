fbf97cb5b3822ebbcd49f0c3f73c10a97866196e
capinemo <capitan__nemo@mail.ru>
2019-04-19 22:17:47 +0300

    Added tests for loadState, saveState, cleanState methods of Storage
    Not it's returns errors because tests not emulate window.localStorage and
    window.document

--- test/list/storage.test.js
+++ test/list/storage.test.js
@@ -12,6 +12,31 @@ module.exports = function () {
             });
         });
 
+        describe(".saveState([1, 2, 3])", function() {
+            it("exeption: Error [do not know how to save state] (2)", function() {
+                assert.throws(
+                    () => Storage.saveState([1, 2, 3])
+                    , /Storage.saveState: do not know how to save state/
+                );
+            });
+        });
 
+        describe(".loadState()", function() {
+            it("exeption: Error [do not know how to load state] (3)", function() {
+                assert.throws(
+                    () => Storage.loadState()
+                    , /Storage.loadState: do not know how to load state/
+                );
+            });
+        });
+
+        describe(".cleanState()", function() {
+            it("exeption: Error [do not know how to clean state] (4)", function() {
+                assert.throws(
+                    () => Storage.cleanState()
+                    , /Storage.cleanState: do not know how to clean state/
+                );
+            });
+        });
     });
 };
